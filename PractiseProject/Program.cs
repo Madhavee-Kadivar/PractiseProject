f29dbccf252f11a72d6791aca217a2ede7d4efb5
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-20 17:54:08 +0530

    [Madhavee] Practise Project

---
+++ PractiseProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Practise Project");
+            //Console.WriteLine("Adding just one line from local");
+            //Console.WriteLine("Adding just one line from remote");
+            //Console.WriteLine("Creating conflict from local");
+            //Console.WriteLine("Creating conflict from remote");
+            Human human = new Human();
+            human.HumanDetails();
+            Human.Speak();
+
+
+            SwapTwoNumbers swapTwoNumbers = new SwapTwoNumbers();
+            swapTwoNumbers.SwapNumbers();
+           
+            Console.Read();
+
+        }
+    }
+}
+
